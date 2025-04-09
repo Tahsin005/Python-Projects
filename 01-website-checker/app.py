de805bb5a0b363451cb9628c50d8e03d8271487b
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-04-10 04:24:53 +0600

    website checker

---
+++ 01-website-checker/app.py
@@ -0,0 +1,10 @@
+print("🔎 WEBSITE URL CHECKER")
+
+url = input("\n🪩 Enter website URL: ")
+
+if url.startswith("https://"):
+    print("🔐 This website uses HTTPS (secure)")
+elif url.startswith("http://"):
+    print("👀 This website uses HTTP (not secure)")
+else:
+    print("❌ This doesn't look like a complete URL")
